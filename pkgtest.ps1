e6d6b43ed29239fc5b500e9c901e3f880b6d3c34
Alicia <smushyaa@gmail.com>
2023-11-30 18:47:25 -0500

    deleted unnecessary files

--- pkgtest.ps1
+++
@@ -1,4 +0,0 @@
-Set-PSDebug -Trace 1
-.\build\debug\liquid-packager.exe create ./resources/test.manifest --enum-name ResourceID --header-output ./generated/resource.h --verbose --memory-usage
-Set-PSDebug -Trace 0
-
